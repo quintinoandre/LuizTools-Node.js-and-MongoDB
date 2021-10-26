f0fcf5c4a764e058e0dcac382247a6c473bff213
André Quintino <pandrequintino@gmail.com>
2021-10-27 00:55:09 +0100

    Aula 01 - exercise 02

---
+++ Exercises/Aula-01/02/index.js
@@ -0,0 +1,11 @@
+const makePerson = (name, age) => {
+	return { name, age };
+};
+
+persons = [];
+
+for (let index = 0; index < 10; index++) {
+	persons.push(makePerson(`person${index + 1}`, 20 + index));
+}
+
+console.log(persons);
