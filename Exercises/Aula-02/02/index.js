ff507ca0e65cc8072d17ccf786ff320154de2293
André Quintino <pandrequintino@gmail.com>
2021-10-28 14:20:25 +0100

    Aula 02 - exercise 02

---
+++ Exercises/Aula-02/02/index.js
@@ -0,0 +1,5 @@
+const verifyEvenOdd = require('./verifyEvenOdd');
+
+console.log(verifyEvenOdd(2));
+
+console.log(verifyEvenOdd(3));
---
+++ Exercises/Aula-02/02/verifyEvenOdd.js
@@ -0,0 +1,4 @@
+module.exports = (number) => {
+	if (number % 2 === 0) console.log('This number is even!');
+	else if (number % 2 === 1) console.log('This number is odd!');
+};
