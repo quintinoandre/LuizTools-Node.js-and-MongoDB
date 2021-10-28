32856b05180fc7fb7d9e2aac2aa290a67776da7f
André Quintino <pandrequintino@gmail.com>
2021-10-28 14:12:25 +0100

    Aula 02 - exercise 01

---
+++ Exercises/Aula-02/01/calculator.js
@@ -0,0 +1,17 @@
+const sum = (num1, num2) => {
+	return num1 + num2;
+};
+
+const subtraction = (minuend, subtrahend) => {
+	return minuend - subtrahend;
+};
+
+const multiplication = (num1, num2) => {
+	return num1 * num2;
+};
+
+const division = (dividend, divider) => {
+	return dividend / divider;
+};
+
+module.exports = { sum, subtraction, multiplication, division };
---
+++ Exercises/Aula-02/01/index.js
@@ -0,0 +1,9 @@
+const { sum, subtraction, multiplication, division } = require('./calculator');
+
+console.log(sum(1, 2));
+
+console.log(subtraction(2, 1));
+
+console.log(multiplication(2, 2));
+
+console.log(division(4, 2));
