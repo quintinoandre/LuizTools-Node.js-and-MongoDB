40e7d32aed1739f4ef062051ffc24b0c7bf3533c
André Quintino <pandrequintino@gmail.com>
2021-10-31 18:11:44 +0000

    M02: A04: crud-04

--- aula2/routes/index.js
+++ aula2/routes/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { findCustomers } = require('../db');
+const { findCustomers, insertCustomer } = require('../db');
 
 /* GET home page. */
 /* router.get('/', (req, res, next) => {
@@ -24,6 +24,24 @@ router.get('/', (req, res, next) => {
 
 router.get('/new', (request, response) => {
 	response.render('customer', { title: 'Customer Registration' });
+});
+
+router.post('/new', ({ body }, response) => {
+	if (!body.name)
+		return response.redirect('/new?error=The name field is required');
+
+	if (body.age && /[0-9]+/.test(body.age))
+		return response.redirect('/new?error=The age field is numeric');
+
+	const { name, city } = body;
+
+	const age = parseInt(body.age);
+
+	const state = body.state.length > 2 ? '' : body.state;
+
+	insertCustomer({ name, age, city, state })
+		.then((result) => response.redirect('/'))
+		.catch((error) => console.log(error));
 });
 
 module.exports = router;
