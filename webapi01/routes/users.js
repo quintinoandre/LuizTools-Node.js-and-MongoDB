36ba2c9bcd922e725533642d15108b3aa1543355
André Quintino <pandrequintino@gmail.com>
2021-11-04 23:07:32 +0000

    M03: A01: WebAPI 04

--- webapi01/routes/users.js
+++ webapi01/routes/users.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { findUsers, insertUser } = require('../db');
+const { findUsers, insertUser, updateUser, deleteUser } = require('../db');
 
 /* GET users listing. */
 router.get('/', (req, res, next) => {
@@ -10,7 +10,23 @@ router.get('/', (req, res, next) => {
 router.post('/', ({ body }, response) => {
 	const user = insertUser(body);
 
-	response.statusCode(201).json(user);
+	response.status(201).json(user);
+});
+
+router.put('/:id', ({ params, body }, response) => {
+	const { id } = params;
+
+	const user = updateUser(id, body);
+
+	response.status(200).json(user);
+});
+
+router.delete('/:id', ({ params, body }, response) => {
+	const { id } = params;
+
+	const user = deleteUser(id);
+
+	response.status(200).json(user);
 });
 
 module.exports = router;
