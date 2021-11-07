d56c78ab5e2b7d5aa3041692c216a7223c61a0cd
André Quintino <pandrequintino@gmail.com>
2021-11-07 23:49:36 +0000

    M03: A02: fs 01

--- webapi01/db.js
+++ webapi01/db.js
@@ -1,39 +1,60 @@
 const { v4: generateId } = require('uuid');
+const fs = require('fs');
 
-global.users = [];
+const FILE_PATH = require('path').join(__dirname, 'users.json');
 
 const findUsers = () => {
-	return global.users;
+	try {
+		return require('./users.json');
+	} catch (ex) {
+		return [];
+	}
 };
 
 const findUser = (id) => {
-	return global.users.find((item) => item.id === id);
+	return findUsers().find((item) => item.id === id);
 };
 
 const insertUser = (user) => {
+	const users = findUsers();
+
 	user.id = generateId();
 
-	global.users.push(user);
+	users.push(user);
+
+	fs.writeFileSync(FILE_PATH, JSON.stringify(users));
 
 	return user;
 };
 
 const updateUser = (id, user) => {
-	return global.users.forEach((item, index, array) => {
+	const users = findUsers();
+
+	users.forEach((item, index, array) => {
 		if (item.id === id) {
 			user.id = id;
 
 			array[index] = user;
 		}
 	});
+
+	fs.writeFileSync(FILE_PATH, JSON.stringify(users));
+
+	return user;
 };
 
 const deleteUser = (id) => {
-	return global.users.forEach((item, index, array) => {
+	const users = findUsers();
+
+	users.forEach((item, index, array) => {
 		if (item.id === id) {
 			array.splice(index, 1);
 		}
 	});
+
+	fs.writeFileSync(FILE_PATH, JSON.stringify(users));
+
+	return id;
 };
 
 module.exports = { findUsers, findUser, insertUser, updateUser, deleteUser };
