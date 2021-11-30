72b878f6eaa2c46e8c6b95090c072acbef705042
André Quintino <pandrequintino@gmail.com>
2021-11-30 13:55:17 +0000

    M04: A04: 02 – Repositório

---
+++ cinema-microservice/movies-service/src/repository/repository.js
@@ -0,0 +1,29 @@
+const { connect } = require('../config/database');
+const { ObjectId } = require('mongodb');
+
+async function getAllMovies() {
+	const db = await connect();
+
+	return db.collection('movies').find().toArray();
+}
+
+async function getMovieById(id) {
+	const db = await connect();
+
+	return db.collection('movies').findOne({ _id: new ObjectId(id) });
+}
+
+async function getMoviesPremieres() {
+	const monthAgo = new Date();
+
+	monthAgo.setMonth(-1);
+
+	const db = await connect();
+
+	return db
+		.collection('movies')
+		.find({ dataLancamento: { $gte: monthAgo } })
+		.toArray();
+}
+
+module.exports = (getAllMovies, getMovieById, getMoviesPremieres);
