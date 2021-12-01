148cd0a85e381e5c2334eb4c8a924113aa9b1298
André Quintino <pandrequintino@gmail.com>
2021-12-01 15:29:48 +0000

    M04: A04: 09 – Finalizando a API

--- cinema-microservice/movies-service/src/index.js
+++ cinema-microservice/movies-service/src/index.js
@@ -1,2 +1,11 @@
-//index.js
-console.log('start');
+const movies = require('./api/movies');
+const repository = require('./repository/repository');
+const { start } = require('./server/server');
+
+(async () => {
+	try {
+		await start(movies, repository);
+	} catch (error) {
+		console.error(error);
+	}
+})();
