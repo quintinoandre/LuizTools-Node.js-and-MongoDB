fab390a9d288627732d9bbe3d37a345bd181c263
André Quintino <pandrequintino@gmail.com>
2021-12-03 17:35:38 +0000

    M04: A05: 04 – Dados Exemplo

--- cinema-microservice/cinema-catalog-service/src/repository/__mocks__/repository.js
+++ cinema-microservice/cinema-catalog-service/src/repository/__mocks__/repository.js
@@ -1,43 +1,198 @@
-const movies = [
+const cinemaCatalog = [
 	{
-		_id: '61a5286bf8943f587e1e01de',
-		titulo: 'Os Vingadores: Ultimato',
-		sinopse: 'Os heróis mais poderosos da Terra enfrentando o Thanos. De novo.',
-		duracao: 181,
-		dataLancamento: new Date('2021-11-30T00:00:00Z'),
-		imagem:
-			'https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_UX182_CR0,0,182,268_AL_.jpg',
-		categorias: ['Aventura', 'Ação'],
+		cidade: 'Gravataí',
+		uf: 'RS',
+		cinemas: [],
 	},
 	{
-		_id: '61a5286bf8943f587e1e01df',
-		titulo: 'Os Vingadores: Guerra Infinita',
-		sinopse: 'Os heróis mais poderosos da Terra enfrentando o Thanos',
-		duracao: 149,
-		dataLancamento: new Date('2018-04-26T00:00:00Z'),
-		imagem:
-			'https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_UX182_CR0,0,182,268_AL_.jpg',
-		categorias: ['Aventura', 'Ação'],
-	},
-	{
-		_id: '61a5286bf8943f587e1e01e0',
-		titulo: 'Os Vingadores: Era de Ultron',
-		sinopse: 'Os heróis mais poderosos da Terra enfrentando o Ultron',
-		duracao: 141,
-		dataLancamento: new Date('2015-04-23T00:00:00Z'),
-		imagem:
-			'https://m.media-amazon.com/images/M/MV5BMTM4OGJmNWMtOTM4Ni00NTE3LTg3MDItZmQxYjc4N2JhNmUxXkEyXkFqcGdeQXVyNTgzMDMzMTg@._V1_UX182_CR0,0,182,268_AL_.jpg',
-		categorias: ['Aventura', 'Ação'],
-	},
-	{
-		_id: '61a5286bf8943f587e1e01e1',
-		titulo: 'Os Vingadores',
-		sinopse: 'Os heróis mais poderosos da Terra enfrentando o Loki',
-		duracao: 143,
-		dataLancamento: new Date('2012-04-27T00:00:00Z'),
-		imagem:
-			'https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_UX182_CR0,0,182,268_AL_.jpg',
-		categorias: ['Aventura', 'Ação'],
+		cidade: 'Porto Alegre',
+		uf: 'RS',
+		pais: 'BR',
+		cinemas: [
+			{
+				_id: new ObjectId(),
+				nome: 'Cinemark Bourbon Ipiranga',
+				salas: [
+					{
+						nome: 1,
+						sessoes: [
+							{
+								data: new Date('2021-12-03T09:00:00Z'),
+								idFilme: '61a5286bf8943f587e1e01df',
+								filme: 'Vingadores: Guerra Infinita',
+								valor: 25.0,
+								assentos: [
+									{
+										numero: 1,
+										disponivel: true,
+									},
+									{
+										numero: 2,
+										disponivel: false,
+									},
+								],
+							},
+							{
+								data: new Date('2021-12-03T11:00:00Z'),
+								idFilme: new ObjectId('61a5286bf8943f587e1e01df'),
+								filme: 'Vingadores: Guerra Infinita',
+								valor: 25.0,
+								assentos: [
+									{
+										numero: 1,
+										disponivel: true,
+									},
+									{
+										numero: 2,
+										disponivel: true,
+									},
+								],
+							},
+							{
+								data: new Date('2021-12-03T13:00:00Z'),
+								idFilme: new ObjectId('61a5286bf8943f587e1e01e0'),
+								filme: 'Vingadores: Era de Ultron',
+								valor: 20.0,
+								assentos: [
+									{
+										numero: 1,
+										disponivel: true,
+									},
+									{
+										numero: 2,
+										disponivel: false,
+									},
+									{
+										numero: 2,
+										disponivel: true,
+									},
+								],
+							},
+						],
+					},
+					{
+						nome: 2,
+						sessoes: [
+							{
+								data: new Date('2021-12-03T09:00:00Z'),
+								idFilme: new ObjectId('61a5286bf8943f587e1e01e0'),
+								filme: 'Vingadores: Era de Ultron',
+								valor: 25.0,
+								assentos: [
+									{
+										numero: 1,
+										disponivel: true,
+									},
+									{
+										numero: 2,
+										disponivel: false,
+									},
+								],
+							},
+							{
+								data: new Date('2021-12-03T11:00:00Z'),
+								idFilme: new ObjectId('61a5286bf8943f587e1e01de'),
+								filme: 'Vingadores: Ultimato',
+								valor: 25.0,
+								assentos: [
+									{
+										numero: 1,
+										disponivel: true,
+									},
+									{
+										numero: 2,
+										disponivel: true,
+									},
+								],
+							},
+							{
+								data: new Date('2021-12-03T13:00:00Z'),
+								idFilme: new ObjectId('61a5286bf8943f587e1e01de'),
+								filme: 'Vingadores: Ultimato',
+								valor: 20.0,
+								assentos: [
+									{
+										numero: 1,
+										disponivel: true,
+									},
+									{
+										numero: 2,
+										disponivel: false,
+									},
+									{
+										numero: 2,
+										disponivel: true,
+									},
+								],
+							},
+						],
+					},
+				],
+			},
+			{
+				_id: new ObjectId(),
+				nome: 'GNC Lindóia',
+				salas: [
+					{
+						nome: 100,
+						sessoes: [
+							{
+								data: new Date('2021-12-03T19:00:00Z'),
+								idFilme: new ObjectId('61a5286bf8943f587e1e01de'),
+								filme: 'Vingadores: Ultimato',
+								valor: 25.0,
+								assentos: [
+									{
+										numero: 1,
+										disponivel: true,
+									},
+									{
+										numero: 2,
+										disponivel: false,
+									},
+								],
+							},
+							{
+								data: new Date('2021-12-03T11:00:00Z'),
+								idFilme: new ObjectId('61a5286bf8943f587e1e01de'),
+								filme: 'Vingadores: Ultimato',
+								valor: 25.0,
+								assentos: [
+									{
+										numero: 1,
+										disponivel: true,
+									},
+									{
+										numero: 2,
+										disponivel: true,
+									},
+								],
+							},
+							{
+								data: new Date('2021-12-03T13:00:00Z'),
+								idFilme: new ObjectId('61a5286bf8943f587e1e01e0'),
+								filme: 'Vingadores: Era de Ultron',
+								valor: 20.0,
+								assentos: [
+									{
+										numero: 1,
+										disponivel: true,
+									},
+									{
+										numero: 2,
+										disponivel: false,
+									},
+									{
+										numero: 2,
+										disponivel: true,
+									},
+								],
+							},
+						],
+					},
+				],
+			},
+		],
 	},
 ];
 
