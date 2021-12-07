60706278595ba37641c549a06e43ba9b5d371d3c
André Quintino <pandrequintino@gmail.com>
2021-12-07 16:18:07 +0000

    M04: A07: 02- API Gateway

---
+++ cinema-microservice/api-gateway/index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const httpProxy = require('express-http-proxy');
+const cookieParser = require('cookie-parser');
+const morgan = require('morgan');
+const helmet = require('helmet');
+
+const { MOVIES_API, CATALOG_API, PORT } = process.env;
+
+const app = express();
+
+app.use(morgan('dev'));
+
+app.use(helmet());
+
+app.use(cookieParser());
+
+const options = {
+	proxyReqPathResolver: ({ originalUrl }) => {
+		return originalUrl;
+	},
+};
+
+const moviesServiceProxy = httpProxy(MOVIES_API, options);
+
+const catalogServiceProxy = httpProxy(CATALOG_API, options);
+
+app.use('/movies', moviesServiceProxy);
+
+app.get(/cities|cinemas/i, catalogServiceProxy);
+
+app.listen(PORT, () => {
+	console.log(`API Gateway started at ${PORT}`);
+});
